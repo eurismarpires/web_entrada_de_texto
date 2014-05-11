6acdbf511359289eeb071172d452e0c0c2dab413
Eurismar Pires Borges <eurismarpires@gmail.com>
2014-05-10 22:50:06 -0300

    Alterei para usar div ao invés de select. Várias outras alterações também

--- script.js
+++ script.js
@@ -1,105 +1,182 @@
-var estados = [
-  "Acre",
-  "Alagoas",
-  "Amazonas",
-  "Amapá",
-  "Bahia",
-  "Distrito Federal",
-  "Ceará",
-  "Espírito Santo",
-  "Goiás",
-  "Maranhão",
-  "Mato Grosso",
-  "Mato Grosso do Sul",
-  "Minas Gerais",
-  "Pará",
-  "Paraíba",
-  "Pernambuco",
-  "Paraná",
-  "Piauí",
-  "Rio de Janeiro",
-  "Rio Grande do Norte",
-  "Rio Grande do Sul",
-  "Roraima",
-  "Rondônia",
-  "Santa Catarina",
-  "São Paulo",
-  "Sergipe", 
-  "Tocantins"
-];
+var teclou_control = false;
+var itemSelecionado;
+window.onload = function(){
 
-
-window.onload = function()
-{
-	document.getElementById("txtBusca").onkeypress = function(){
-	  if (event.ctrlKey && event.keyCode == 32) {
-      criarLista();
-	  }
-    if (event.ctrlKey)
-    return false;	 	  
-	}
-	document.getElementById("lista_de_estados").onkeypress = function(){
-	  //if (event.keyCode == 13) {
-      alert("você teclou enter....");
-	  //}
-	}	
-/*	document.getElementById("txtBusca").onkeyup = function(){
-	  criarLista();
-	}
-	
-*/	
-}
-
-function criarLista() 
-{
-  var busca = document.getElementById("txtBusca");
-  var texto = busca.value;  
-  var tamTexto = texto.length;
-  
-  var elem = document.getElementById("lista_de_estados");
-    
-  if(elem !== null){
-     document.body.removeChild(elem);
+  document.getElementById("txtBusca").onkeypress = function(){
+     if (event.ctrlKey && event.keyCode == 32) {
+      listar();
+      teclou_control = true;
+      return false;
+    }
   }
-    var x = document.createElement("SELECT");
-    x.setAttribute("id", "lista_de_estados");
-    x.setAttribute("size", "10");
-    x.setAttribute("onkeypress","selecionar()");
-    document.body.appendChild(x);
+  document.getElementById("txtBusca").onkeyup = function(){
+    var busca = document.getElementById("txtBusca");
+    var texto = busca.value;
+    var tamTexto = texto.length;
+    
+     if (event.ctrlKey && event.keyCode == 32) {
+      listar();
+      //criarParagrafo("vou tentar aki.............................");
+      teclou_control = true;
+      return false;
+    }    
+    
+  /*  
+    if(tamTexto > 0 && teclou_control){
+      listar();
+      return false;
+    }
+  */  
     
     
-      for (var i = 0; i < estados.length; i++){
-        if(tamTexto > 0){
+    if(event.keyCode == 27) //tecla Esc
+    {
+      limparLista();
+      teclou_control = false;
+     // criarParagrafo("condição verdadeira");
+    }else{
+      //criarParagrafo("condição falsa");
+    }    
+    
+  } 
+  document.getElementById("txtBusca").onkeydown = function(){
+    var divEstados = document.getElementById("lista_de_estados"); 
+    
+    
+     if (event.ctrlKey && event.keyCode == 32){
+      teclou_control = true;
+      return false;
+    }    
+    
+    
+    if(event.keyCode == 38){ //seta p/ cima
+      if(itemSelecionado != divEstados.firstElementChild){
+        itemSelecionado = itemSelecionado.previousSibling;
+        posicionar(divEstados);
+      }
+    }
+    if(event.keyCode == 40){ //seta para baixo
+      if(itemSelecionado != divEstados.lastElementChild){
+        itemSelecionado = itemSelecionado.nextSibling;
+        posicionar(divEstados);
+      }
+    }
+    if(event.keyCode == 13){
+      document.getElementById("txtBusca").value = itemSelecionado.innerHTML;
+      limparLista();
+      teclou_control = false;
+     // return false;
+    }
+  
+  
+  
+  
+  
+  
+  
+  
+   // var textarea = document.getElementById("txtBusca");  
+  //  var pos = textarea.selectionStart;
+//    criarParagrafo(pos);
+      
+      
+      
+      
+      
+      
+      
+      
+      
+     
+  }   
+  document.getElementById("txtBusca").onclick = function(){
+    // criarParagrafo("onclick");
+  } 
+  document.getElementById("lista_de_estados").onkeydown = function(){
+    if(event.keyCode == 13) 
+     criarParagrafo("teclou enter......");
+  }   
+}
+function posicionar(divEstados){
+  itemSelecionado.className = "atual";
+  for(var i = 0; i < divEstados.childNodes.length; i++){
+    var divUF = divEstados.childNodes[i];
+    if(divUF != itemSelecionado)
+      divUF.className = "";
+    }     
+}
+function criarParagrafo(txt){
+  var p = document.createElement("p");
+  p.innerHTML = txt;
+  p.style.top = 300;
+  document.body.appendChild(p); 
+}
+function selecionarItem(obj){
+  var busca = document.getElementById("txtBusca");
+  busca.value = obj.innerHTML;
+  limparLista();
+}
+function mouseEntrar(obj){
+  obj.className = "atual";
+}
+function mouseSair(obj){
+  var divEstados = document.getElementById("lista_de_estados");
+  for(var i = 0; i < divEstados.childNodes.length; i++){
+    var divUF = divEstados.childNodes[i];
+    divUF.className = "";
+  }
+}
+function listar(){
+    var busca = document.getElementById("txtBusca");
+    var texto = busca.value;
+    
+    
+    
+    
+    var tamTexto = texto.length;
+    tamTexto = busca.selectionStart;
+    
+    
+    
+    
+    limparLista();
+    //adiciona a div principal
+    var divPrincipal = document.createElement("DIV");
+    divPrincipal.setAttribute("id","lista_de_estados");
+    divPrincipal.setAttribute("class","suggestions");
+    document.body.appendChild(divPrincipal);
+    //adiciona os estados na div principal
+    for(var i = 0; i < estados.length; i++){
+      if(tamTexto > 0){
         var subEstado = estados[i].substr(0,tamTexto);
         if(texto.toLocaleUpperCase() == subEstado.toLocaleUpperCase()){
-          var uf = document.createElement("option");
-          uf.setAttribute("value", i);
-          var nomeEstado = document.createTextNode(estados[i]);        
-          uf.appendChild(nomeEstado);
-          document.getElementById("lista_de_estados").appendChild(uf);
-        }
-          
-        }else{
-          var uf = document.createElement("option");
-          uf.setAttribute("value", i);
-          var nomeEstado = document.createTextNode(estados[i]);        
-          uf.appendChild(nomeEstado);
-          document.getElementById("lista_de_estados").appendChild(uf);          
-          
+          criarItens(i);
         }
       }
-  }
- 
-function selecionar(){
-  var x = document.getElementById("lista_de_estados").selectedIndex;
-  var y = document.getElementById("lista_de_estados").options;
-  //alert("Index: " + y[x].index + " is " + y[x].text);
-  var txtBusca = document.getElementById("txtBusca");
-  txtBusca.innerHTML = y[x].text;
+      else{
+        criarItens(i);
+      }
+    }
+    document.getElementById("lista_de_estados").firstElementChild.className = "atual";
+    itemSelecionado = document.getElementById("lista_de_estados").firstElementChild;
 }
-    
-    
-  
+function criarItens(i){
+  var divUF = document.createElement("DIV");
+  divUF.setAttribute("id",i);
+  divUF.setAttribute("onclick","selecionarItem(this)");
+  divUF.setAttribute("onmouseover","mouseEntrar(this)");
+  divUF.setAttribute("onmouseout","mouseSair(this)");
+  txtUF = document.createTextNode(estados[i]);
+  divUF.appendChild(txtUF);
+  document.getElementById("lista_de_estados").appendChild(divUF);  
+}
+function limparLista(){
+    var elem = document.getElementById("lista_de_estados");
+    if(elem !== null){
+      document.body.removeChild(elem);
+    }  
+}
+
   
 
   
